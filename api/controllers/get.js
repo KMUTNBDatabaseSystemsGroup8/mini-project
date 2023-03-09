333156644243359bfee5b05ba2342696c6079f09
Phiraphon Phansa-nga <Phiraphonpn@gmail.com>
2023-03-10 00:21:07 +0700

    finish GET method(posgresql)

--- api/controllers/get.js
+++ api/controllers/get.js
@@ -2,27 +2,83 @@ const { PrismaClient } = require('@prisma/client')
 const db = new PrismaClient()
 
 exports.companyList = async (req, res) => {
-    const companies = await db.companies.findMany({select: {id: true, company_name: true}})
-    res.json(companies)
+    try {
+        const companies = await db.companies.findMany({ select: { id: true, company_name: true } })
+        res.json(companies)
+    } catch (error) {
+        res.status(400);
+        res.json({
+            error: true,
+            message: "Bad Request"
+        });
+    }
 };
 
 exports.jobList = async (req, res) => {
-    const jobs = await db.jobs.findMany({select: {id: true, jobposition : true}})
-    res.json(jobs)
+    try {
+        const jobs = await db.jobs.findMany({ select: { id: true, jobposition: true } })
+        res.json(jobs)
+    } catch (error) {
+        res.status(400);
+        res.json({
+            error: true,
+            message: "Bad Request"
+        });
+    }
 };
 
 exports.getCompanyByID = async (req, res) => {
-    res.json({message:"ขอข้อมูลบริษัท"})
+    try {
+        const company_id = req.params.companyId;
+        const company = await db.companies.findUniqueOrThrow({ where: { id: parseInt(company_id) } })
+        res.json(company)
+    } catch (error) {
+        res.status(444);
+        res.json({
+            error: true,
+            message: "Data not found"
+        });
+    }
 };
 
 exports.getJobByID = async (req, res) => {
-    res.json({message:"ขอข้อมูลงานจาก ID"})
+    try {
+        const job_id = req.params.jobId;
+        const job = await db.jobs.findUniqueOrThrow({ where: { id: parseInt(job_id) } })
+        res.json(job)
+    } catch (error) {
+        res.status(444);
+        res.json({
+            error: true,
+            message: "Data not found"
+        });
+    }
 };
 
 exports.searchByPosition = async (req, res) => {
-    res.json({message:"ขอรายชื่องานจาก keyword ตำแหน่งงานนี้"})
+    try {
+        const job_positions = req.params.position;
+        const job = await db.jobs.findMany({ where: { jobposition: { contains: job_positions, mode: "insensitive" } }, select: { id: true, jobposition : true } })
+        res.json(job)
+    } catch (error) {
+        res.status(444);
+        res.json({
+            error: true,
+            message: "Data not found"
+        });
+    }
 };
 
 exports.searchByEducation = async (req, res) => {
-    res.json({message:"ขอรายชื่องานที่ใช้วุฒิการศึกษานี้"})
+    try {
+        const educations = req.params.education;
+        const job = await db.jobs.findMany({ where: { education: { contains: educations, mode: "insensitive" } } })
+        res.json(job)
+    } catch (error) {
+        res.status(444);
+        res.json({
+            error: true,
+            message: "Data not found"
+        });
+    }
 };
