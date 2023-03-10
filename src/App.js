df19558c19d88db05a9e115c7377d51ed451cfd4
wannipaa <s6401012630116@email.kmutnb.ac.th>
2023-03-10 21:29:05 +0700

    UI JobDetail

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './JobDetail.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './NavbarComp';
 import Home from './Home';
@@ -9,6 +10,17 @@ function App() {
         <NavbarComp />
         <Home/>
       </div>
+  );
+}
+
+function JobDetail() {
+  return (
+    <div className="content">
+      <p>ตำแหน่งที่รับสมัคร</p>
+      <p>ชื่อเต็มของบ.</p>
+      <p>ที่ตั้งบ.</p>
+      <p>ลงประกาศเมื่อ - ชม.ที่ผ่านมา</p>
+    </div>
   );
 }
 
