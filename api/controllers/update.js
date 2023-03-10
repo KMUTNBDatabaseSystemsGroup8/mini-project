959828f7d1524e288fbc8cab345b923e6ba227d5
JedsadaSrijunpoe <tae456852@gmail.com>
2023-03-10 17:41:46 +0700

    finish UPDATE method(postgresql)

--- api/controllers/update.js
+++ api/controllers/update.js
@@ -2,9 +2,51 @@ const { PrismaClient } = require('@prisma/client')
 const db = new PrismaClient()
 
 exports.updateJob = async (req, res) => {
-    res.json({message:"อัปเดทงานใหม่"})
+    try{
+        const job_id = req.body.id;
+        const job = await db.jobs.update({
+             where: { id: parseInt(job_id) },
+             data: {
+                jobposition : req.body.jobposition,
+			    education : req.body.education,
+			    toeic : parseInt(req.body.toeic),
+			    gpax : parseFloat(req.body.gpax),
+                availablecount : parseInt(req.body.availablecount),
+			    jobrequirement :  req.body.jobrequirement,
+			    salary : req.body.salary,
+			    company_id : parseInt(req.body.company_id),
+             },
+            })
+        res.json(job)
+    } catch(error) {
+        res.status(444);
+        res.json({
+            error: true,
+            message: "Bad Request"
+        });
+    }
 };
 
 exports.updateCompany = async (req, res) => {
-    res.json({message:"อัปเดทข้อมูลบริษัท"})
+    try{
+        const company_id = req.body.id;
+        const company = await db.companies.update({
+             where: { id: parseInt(company_id) },
+             data: {
+                company_name : req.body.company_name,
+			    location : req.body.location,
+			    email : req.body.email,
+			    marketcap : req.body.marketcap,
+                website : req.body.website,
+			    telephone :  req.body.telephone,
+             },
+            })
+        res.json(company)
+    } catch(error) {
+        res.status(444);
+        res.json({
+            error: true,
+            message: "Bad Request"
+        });
+    }
 };
