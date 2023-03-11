a0516acfc55aa34e62614656cc89296136854cda
benz <benzbk1109@gmail.com>
2023-03-11 18:00:31 +0700

    finish POST method(postsql)

--- api/controllers/create.js
+++ api/controllers/create.js
@@ -2,9 +2,32 @@ const { PrismaClient } = require('@prisma/client')
 const db = new PrismaClient()
 
 exports.createCompany = async (req, res) => {
-    res.json({message:"สร้างบริษัท"})
+    try{
+        const company = await db.companies.create({
+             data: req.body,
+            })
+        res.json(company)
+    } catch(error) {
+        res.status(400);
+        res.json({
+            error: true,
+            message: "Bad Request Or at least company_name,location,email and telephone are required. "
+        });
+    }
 };
 
 exports.createJob = async (req, res) => {
-    res.json({message:"สร้างงานใหม่"})
+    try{
+        const job = await db.jobs.create({
+             data: req.body,
+            })
+        const company = await db.companies.findUniqueOrThrow({ where: { id: parseInt(job.company_id) } })
+        res.json(job)
+    } catch(error) {
+        res.status(400);
+        res.json({
+            error: true,
+            message: "Bad Request Or at least jobposition,availablecount,jobrequirement and company_id are required. "
+        });
+    }
 };
